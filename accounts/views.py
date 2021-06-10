44a461b7a7e70d40bcd7e0ba445c4a619e16cf9a
vasu gangavarapu <vasu.egnify@gmail.com>
2021-06-10 14:45:17 +0530

    change again

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
-
 from django.db.models import Func,F
+from rest_framework.views import APIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
